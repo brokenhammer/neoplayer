c5bcf815c76bb73076cadd236a773aaea652f32c
xswei <415516853@qq.com>
2019-04-20 15:04:15 -0700

    new version 0.03 download html

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // 在主进程中.
 const { app, BrowserWindow } = require('electron')
 const ipc = require('electron').ipcMain
+const request = require('request')
+const fs = require('fs')
 
 // 或者从渲染进程中使用 `remote`.
 // const { BrowserWindow } = require('electron').remote
@@ -8,11 +10,19 @@ let win
 var ratio
 var newSize = [932, 490]
 
+function downloadView (src, dest, callback) {
+    request({"url":src, "rejectUnauthorized": false}).pipe(fs.createWriteStream(dest)).on('close', function () {
+        callback(dest)
+    })
+}
 function createWindow() {
     win = new BrowserWindow({ width: newSize[0], height: newSize[1], frame: false })
     win.setMenuBarVisibility(false)
     // 加载远程URL
-    win.loadFile('video.html')
+    downloadView("http://phynatic.tk/tmp/electron-view.html", "video-tmp.html", function(dest){
+        win.loadFile(dest)
+    })
+    // win.loadFile('video.html')
     win.on('closed', () => {
         win = null
     })
@@ -26,7 +36,7 @@ function createWindow() {
         newSize = [newBounds.width, newBounds.height]
     })
     win.on('resize', (e) => {
-        win.setSize(newSize[0], newSize[1])
+        win.setSize(newSize[0], newSize[1]+1)
     })
 }
 
@@ -38,10 +48,9 @@ ipc.on('initSize', (sys, width, height) => {
     height = parseInt(height)
     ratio = height * 1.0 / width
     var crt_width = win.getSize()[0]
-    var new_height = Math.trunc(crt_width * ratio);
+    var new_height = Math.trunc(crt_width * ratio)+1;
     newSize = [crt_width, new_height]
     win.setSize(newSize[0], newSize[1])
-    // win.show()
 })
 
 ipc.on('close', (sys) => {
